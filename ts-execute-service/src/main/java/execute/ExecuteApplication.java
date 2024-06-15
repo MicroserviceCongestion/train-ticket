5f4e986bc45b55b799597e6c26750fda854b6233
LittleEtx <1968717506@qq.com>
2024-06-16 04:28:12 +0800

    Add interceptors since Spring 3.2 will no longer set the CONTENT_LENGTH in header for client request, see https://github.com/spring-projects/spring-framework/issues/31902

--- ts-execute-service/src/main/java/execute/ExecuteApplication.java
+++ ts-execute-service/src/main/java/execute/ExecuteApplication.java
@@ -7,9 +7,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 /**
  * @author fdse
@@ -28,6 +34,13 @@ public class ExecuteApplication {
     @LoadBalanced
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+        var interceptor = new ClientHttpRequestInterceptor() {
+            @Override
+            public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+                request.getHeaders().setContentLength(body.length);
+                return execution.execute(request, body);
+            }
+        };
+        return builder.interceptors(interceptor).build();
     }
 }
--- ts-verification-code-service/src/main/java/verifycode/VerifyCodeApplication.java
+++ ts-verification-code-service/src/main/java/verifycode/VerifyCodeApplication.java
@@ -6,7 +6,13 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 /**
  * @author fdse
@@ -20,6 +26,13 @@ public class VerifyCodeApplication {
     @LoadBalanced
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+        var interceptor = new ClientHttpRequestInterceptor() {
+            @Override
+            public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+                request.getHeaders().setContentLength(body.length);
+                return execution.execute(request, body);
+            }
+        };
+        return builder.interceptors(interceptor).build();
     }
 }
